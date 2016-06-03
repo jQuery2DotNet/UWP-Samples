33a4a48d64b4eacdd06c3df368a6f1896b752a1d
jQuery2DotNet <info@jquery2dotnet.com>
2016-06-03 19:52:18 +0530

    Add title bar color change example

---
+++ Title-Bar-Color-Change/Title-Bar-Color-Change/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Title_Bar_Color_Change
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            SetTitleBarBackground();
+        }
+
+        private void SetTitleBarBackground()
+        {
+            // Get the instance of the Title Bar
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            // Set the color of the Title Bar content
+            titleBar.BackgroundColor = Colors.Orange;
+            titleBar.ForegroundColor = Colors.Green;
+
+            // Set the color of the Title Bar buttons
+            titleBar.ButtonBackgroundColor = Colors.Orange;
+            titleBar.ButtonForegroundColor = Colors.Green;
+        }
+    }
+}
